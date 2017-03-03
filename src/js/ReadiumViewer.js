3f275618e96cf1539af7b7d5a5963a53694bbb4d
Andy Hubert <andy.hubert@gmail.com>
2017-03-03 09:49:06 +0200

    Install sentry.io

--- src/js/ReadiumViewer.js
+++ src/js/ReadiumViewer.js
@@ -3,6 +3,8 @@ define(['jquery', './EpubLibrary', './EpubReader', 'readium_shared_js/helpers', 
     var _initialLoad = true; // replaces pushState() with replaceState() at first load 
     var initialLoad = function(){
 
+        if(typeof Raven != 'undefined') Raven.config('https://0569beced42c4b068367c8d47cfddf36@sentry.io/144504').install()
+
         Settings.initialize(function() {  // biblemesh_ : this wrapper is new
 
             Settings.patchFromLocalStorage(function() {  // biblemesh_ : this wrapper is new
