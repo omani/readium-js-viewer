c08429a7bc614b9e842a2877071cfbc4c908197b
Andy Hubert <andy.hubert@gmail.com>
2017-05-11 12:09:13 +0300

    Updated for multi-tenancy

--- ReadiumData.sql
+++ ReadiumData.sql
@@ -6,8 +6,8 @@
 # https://github.com/sequelpro/sequelpro
 #
 # Host: localhost (MySQL 5.7.17)
-# Database: ReadiumData
-# Generation Time: 2017-03-22 10:42:31 +0000
+# Database: readiumdata
+# Generation Time: 2017-05-11 09:07:13 +0000
 # ************************************************************
 
 
@@ -36,11 +36,22 @@ CREATE TABLE `book` (
 
 
 
+# Dump of table book-idp
+# ------------------------------------------------------------
+
+CREATE TABLE `book-idp` (
+  `book_id` int(11) unsigned NOT NULL,
+  `idp_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`book_id`,`idp_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+
+
 # Dump of table highlight
 # ------------------------------------------------------------
 
 CREATE TABLE `highlight` (
-  `user_id` int(11) unsigned NOT NULL,
+  `user_id` int(11) NOT NULL,
   `book_id` int(11) unsigned NOT NULL,
   `spineIdRef` varchar(150) COLLATE utf8_bin NOT NULL DEFAULT '',
   `cfi` varchar(150) COLLATE utf8_bin NOT NULL DEFAULT '',
@@ -51,7 +62,8 @@ CREATE TABLE `highlight` (
   PRIMARY KEY (`user_id`,`book_id`,`spineIdRef`,`cfi`,`deleted_at`),
   KEY `user_id` (`user_id`,`book_id`),
   KEY `deleted_at` (`deleted_at`),
-  KEY `updated_at` (`updated_at`)
+  KEY `updated_at` (`updated_at`),
+  KEY `user_id_2` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 
@@ -60,19 +72,23 @@ CREATE TABLE `highlight` (
 # ------------------------------------------------------------
 
 CREATE TABLE `idp` (
-  `code` varchar(10) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` text COLLATE utf8_bin NOT NULL,
-  `logoSrc` text COLLATE utf8_bin NOT NULL,
-  `smallLogoSrc` text COLLATE utf8_bin NOT NULL,
-  `entryPoint` text COLLATE utf8_bin NOT NULL,
-  `logoutUrl` text COLLATE utf8_bin NOT NULL,
-  `nameQualifier` varchar(100) COLLATE utf8_bin NOT NULL DEFAULT '',
-  `idpcert` text COLLATE utf8_bin NOT NULL,
-  `spcert` text COLLATE utf8_bin NOT NULL,
-  `spkey` text COLLATE utf8_bin NOT NULL,
+  `domain` varchar(253) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `useReaderTxt` tinyint(4) NOT NULL,
+  `entryPoint` text COLLATE utf8_bin,
+  `logoutUrl` text COLLATE utf8_bin,
+  `nameQualifier` varchar(100) COLLATE utf8_bin DEFAULT '',
+  `idpcert` text COLLATE utf8_bin,
+  `spcert` text COLLATE utf8_bin,
+  `spkey` text COLLATE utf8_bin,
   `language` varchar(5) COLLATE utf8_bin DEFAULT NULL,
-  PRIMARY KEY (`code`),
-  KEY `nameQualifier` (`nameQualifier`)
+  `created_at` datetime NOT NULL,
+  `demo_expires_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `domain` (`domain`),
+  KEY `nameQualifier` (`nameQualifier`),
+  KEY `demo_expires_at` (`demo_expires_at`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 
@@ -81,11 +97,12 @@ CREATE TABLE `idp` (
 # ------------------------------------------------------------
 
 CREATE TABLE `latest_location` (
-  `user_id` int(11) unsigned NOT NULL,
+  `user_id` int(11) NOT NULL,
   `book_id` int(11) unsigned NOT NULL,
   `cfi` varchar(150) COLLATE utf8_bin NOT NULL DEFAULT '',
   `updated_at` datetime(3) NOT NULL,
-  PRIMARY KEY (`user_id`,`book_id`)
+  PRIMARY KEY (`user_id`,`book_id`),
+  KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 
@@ -96,11 +113,11 @@ CREATE TABLE `latest_location` (
 CREATE TABLE `user` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `user_id_from_idp` int(11) NOT NULL,
-  `idp_code` varchar(10) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `idp_id` int(11) unsigned NOT NULL,
   `email` varchar(256) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `last_login_at` datetime NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `user_id_from_idp` (`user_id_from_idp`,`idp_code`),
+  UNIQUE KEY `user_id_from_idp` (`user_id_from_idp`,`idp_id`),
   KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
