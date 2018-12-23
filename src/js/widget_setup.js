0205d591b6ec7199a8adf862db5b5818e2032458
Huberts <huberts@Hubertss-MBP.lan>
2018-12-23 11:54:04 +0200

    Added icon for opening in new tab

--- src/js/widget_setup.js
+++ src/js/widget_setup.js
@@ -70,6 +70,23 @@
                     + '.erasereader-widget-div-dark .widget-reference-a, .erasereader-widget-div-dark .erasereader-widget-forbidden {'
                         + "color: white;"
                     + '}'
+                    + '.new-tab-icon {'
+                        + 'display: none;'
+                    + '}'
+                    + '.widget-reference-a:hover .new-tab-icon {'
+                        + 'background-image: url(\'data:image/svg+xml;utf8,<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512"><title></title><g id="icomoon-ignore"></g><path d="M96 32v384h384v-384h-384zM448 384h-320v-320h320v320zM64 448v-336l-32-32v400h400l-32-32h-336z"></path><path d="M176 128l80 80-96 96 48 48 96-96 80 80v-208z"></path></svg>\');'
+                        + 'display: block;'
+                        + 'position: absolute;'
+                        + 'width: 20px;'
+                        + 'height: 20px;'
+                        + 'top: -20px;'
+                        + 'right: -2px;'
+                        + 'padding: 2px;'
+                        + 'background-size: 20px;'
+                        + 'background-repeat: no-repeat;'
+                        + 'background-position: center;'
+                        + 'background-color: white;'
+                    + '}'
                     + '';
         d.head.appendChild(node);
     }
@@ -175,10 +192,14 @@
                                 className: "widget-author",
                                 innerText: (payload.author || ""),
                             });
+                            var newTabIconEl = newEl('div', {
+                                className: "new-tab-icon",
+                            });
 
                             refElA.appendChild(spineLblEl);
                             refElA.appendChild(titleEl);
                             refElA.appendChild(authorEl);
+                            refElA.appendChild(newTabIconEl);
                             refEl.appendChild(refElA);
                             iframeEl.parentNode.insertBefore(refEl, null);
                             
