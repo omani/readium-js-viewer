bb5db59fdbb0803e70d75ea891476718ad06a158
Huberts <huberts@Hubertss-MBP.lan>
2019-02-15 12:24:31 +0200

    Show loading icon while doing authentication

--- src/js/widget_setup.js
+++ src/js/widget_setup.js
@@ -23,34 +23,35 @@
                         + "border-width: 0 2px 0 17px;"
                         + "margin: 10px 0;"
                         + "padding: 6px 14px;"
+                        + "position: relative;"
                     + '}'
-                    + '.widget-reference {'
+                    + '.erasereader-widget-reference {'
                         + 'text-align: right;'
                     + '}'
-                    + '.widget-reference-a {'
+                    + '.erasereader-widget-reference-a {'
                         + 'display: inline-block;'
                         + 'color: black;'
                         + 'text-decoration: none;'
                         + 'position: relative;'
                     + '}'
-                    + '.widget-reference-a:hover .widget-title {'
+                    + '.erasereader-widget-reference-a:hover .erasereader-widget-title {'
                         + 'text-decoration: underline;'
                     + '}'
-                    + '.widget-spinelabel {'
+                    + '.erasereader-widget-spinelabel {'
                         + 'opacity: .9;'
                         + 'padding-top: 5px;'
                         + 'font-size: .85em;'
                     + '}'
-                    + '.widget-title {'
+                    + '.erasereader-widget-title {'
                         + 'font-weight: bold;'
                         + 'font-size: 1em;'
                     + '}'
-                    + '.widget-author {'
+                    + '.erasereader-widget-author {'
                         + 'opacity: .5;'
                         + 'font-size: 1em;'
                         + 'padding-bottom: 5px;'
                     + '}'
-                    + '.widget-reference::before {'
+                    + '.erasereader-widget-reference::before {'
                         + 'content: "”";'
                         + 'float: right;'
                         + 'font-size: 100px;'
@@ -67,13 +68,13 @@
                         + 'padding: 50px 0;'
                         + 'font-style: italic;'
                     + '}'
-                    + '.erasereader-widget-div-dark .widget-reference-a, .erasereader-widget-div-dark .erasereader-widget-forbidden {'
+                    + '.erasereader-widget-div-dark .erasereader-widget-reference-a, .erasereader-widget-div-dark .erasereader-widget-forbidden {'
                         + "color: white;"
                     + '}'
-                    + '.new-tab-icon {'
+                    + '.erasereader-new-tab-icon {'
                         + 'display: none;'
                     + '}'
-                    + '.widget-reference-a:hover .new-tab-icon {'
+                    + '.erasereader-widget-reference-a:hover .erasereader-new-tab-icon {'
                         + 'background-image: url(\'data:image/svg+xml;utf8,<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512"><title></title><g id="icomoon-ignore"></g><path d="M96 32v384h384v-384h-384zM448 384h-320v-320h320v320zM64 448v-336l-32-32v400h400l-32-32h-336z"></path><path d="M176 128l80 80-96 96 48 48 96-96 80 80v-208z"></path></svg>\');'
                         + 'display: block;'
                         + 'position: absolute;'
@@ -86,6 +87,44 @@
                         + 'background-repeat: no-repeat;'
                         + 'background-position: center;'
                         + 'background-color: white;'
+                    + '}'
+                    + '.erasereader-spinner {'
+                        + 'width: 100px;'
+                        + 'text-align: center;'
+                        + 'position: absolute;'
+                        + 'top: calc(50% - 9px);'
+                        + 'left: calc(50% - 50px);'
+                    + '}'
+                    + '.erasereader-spinner > div {'
+                        + 'width: 12px;'
+                        + 'height: 12px;'
+                        + 'margin: 0 5px;'
+                        + 'background-color: #AAA;'
+                        + 'border-radius: 100%;'
+                        + 'display: inline-block;'
+                        + '-webkit-animation: erasereader-sk-bouncedelay .85s infinite ease-in-out both;'
+                        + 'animation: erasereader-sk-bouncedelay .85s infinite ease-in-out both;'
+                    + '}'
+                    + '.erasereader-spinner .erasereader-bounce1 {'
+                        + '-webkit-animation-delay: -0.17s;'
+                        + 'animation-delay: -0.17s;'
+                    + '}'
+                    + '.erasereader-spinner .erasereader-bounce2 {'
+                        + '-webkit-animation-delay: -0.09s;'
+                        + 'animation-delay: -0.09s;'
+                    + '}'
+                    + '@-webkit-keyframes erasereader-sk-bouncedelay {'
+                        + '0%, 80%, 100% { -webkit-transform: scale(0) }'
+                        + '40% { -webkit-transform: scale(1.0) }'
+                    + '}'
+                    + '@keyframes erasereader-sk-bouncedelay {'
+                        + '0%, 80%, 100% { '
+                            + '-webkit-transform: scale(0);'
+                            + 'transform: scale(0);'
+                        + '} 40% { '
+                            + '-webkit-transform: scale(1.0);'
+                            + 'transform: scale(1.0);'
+                        + '}'
                     + '}'
                     + '';
         d.head.appendChild(node);
@@ -133,6 +172,11 @@
                         + "height: " + (queryParamObj.maxheight.match(/px$/) ? Math.min(queryParamObj.maxheight, 200) : 200) + "px;",
                 });
 
+                var spinnerEl = newEl('div', {
+                    className: 'erasereader-spinner',
+                });
+                spinnerEl.innerHTML = '<div class="erasereader-bounce1"></div><div class="erasereader-bounce2"></div><div class="erasereader-bounce3"></div>';
+
                 var divEl = newEl('div', {
                     className: 'erasereader-widget-div' + (queryParamObj.theme=='night-theme' ? ' erasereader-widget-div-dark' : ''),
                     style: ""
@@ -140,12 +184,14 @@
                 });
 
                 divEl.appendChild(iframeEl);
+                divEl.appendChild(spinnerEl);
                 el.parentNode.replaceChild(divEl, el);
             }
 
             window.addEventListener('message', function(event) {
                 var data = event.data;
                 var iframeEl = d.getElementById(data.iframeid);
+                var spinnerEl = iframeEl.nextSibling;
 
                 if(iframeEl) {
                     switch(data.action) {
@@ -167,33 +213,34 @@
                         
                         case 'loading':
                             iframeEl.style.opacity = "";
+                            spinnerEl.parentNode.removeChild(spinnerEl);
                             break;
 
                         case 'setReference':
                             var payload = data.payload || {};
                             var refEl = newEl('div', {
-                                className: "widget-reference",
+                                className: "erasereader-widget-reference",
                             });
                             var refElA = newEl('a', {
-                                className: "widget-reference-a",
+                                className: "erasereader-widget-reference-a",
                                 target: '_blank',
                                 href: iframeEl.src.replace(/&widget=1.*$/, '&flash=1').replace(/^https?:\/\/[^\/]+/, event.origin),
                             });
                             var spineLblEl = newEl('div', {
-                                className: "widget-spinelabel",
+                                className: "erasereader-widget-spinelabel",
                                 innerText: payload.spineLabel ? ('“' + payload.spineLabel + '”') : "",
                                 // note: it is important I do not inject HTML as I do not check the postMessage source
                             });
                             var titleEl = newEl('div', {
-                                className: "widget-title",
+                                className: "erasereader-widget-title",
                                 innerText: (payload.title || ""),
                             });
                             var authorEl = newEl('div', {
-                                className: "widget-author",
+                                className: "erasereader-widget-author",
                                 innerText: (payload.author || ""),
                             });
                             var newTabIconEl = newEl('div', {
-                                className: "new-tab-icon",
+                                className: "erasereader-new-tab-icon",
                             });
 
                             refElA.appendChild(spineLblEl);
